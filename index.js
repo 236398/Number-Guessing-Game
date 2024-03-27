5b9007e8f6ed6db076e6e01c2bcffe3f41a1509c
Muhammad Hasan <hasantariq4347@gmail.com>
2024-03-27 17:33:00 +0500

    number guuessing game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+console.log("Guess s number between 1 to 8");
+// Now create variable
+let a = Math.floor(Math.random() * 7);
+import inquirer from "inquirer";
+while (true) {
+    let input = await inquirer.prompt(({ name: "guess", type: "number",
+        message: "Enter your number between 1 to 8:" }));
+    //provide code
+    let ans = input.guess;
+    if (a == ans) {
+        console.log("congratulation your guess number is correct");
+        break;
+    }
+    else {
+        console.log("sorry you guess wrong number try again");
+    }
+}
